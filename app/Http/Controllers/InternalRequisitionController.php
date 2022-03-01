c7dd98840f86f504be4225fb891e56f39687be97
Dwayne Beckett <dwayneb@serha.gov.jm>
2022-03-01 12:18:42 -0500

    fix ipr error

--- app/Http/Controllers/InternalRequisitionController.php
+++ app/Http/Controllers/InternalRequisitionController.php
@@ -245,7 +245,7 @@ class InternalRequisitionController extends Controller
                 // $sub_users = User::users_in_institution($internal_requisition->institution_id)->whereIn('role_id',[10,11]);
                 // $sub_users->each->notify(new InternalRequisitionPublish($internal_requisition));
                 $add_role_user = User::user_with_roles(auth()->user()->institution_id,auth()->user()->department_id,13);
-                $add_role_user->each->notify(new InternalRequisitionPublish($internalRequisition));
+                $add_role_user->each->notify(new InternalRequisitionPublish($internal_requisition));
 
 
             }
