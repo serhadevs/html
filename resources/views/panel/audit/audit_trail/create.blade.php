6fbfef97059e56e5ed6b61e138ccccdffab14826
Dwayne Beckett <dwayneb@serha.gov.jm>
2023-01-24 14:47:22 -0500

    add export button

--- resources/views/panel/audit/audit_trail/create.blade.php
+++ resources/views/panel/audit/audit_trail/create.blade.php
@@ -124,6 +124,14 @@
 <script src="/js/pages/dashboard.min.js"></script>
  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
  <script src="/js/sweet/sweetalert.min.js"></script> 
+ <script src="https://cdn.datatables.net/fixedcolumns/4.0.0/js/dataTables.fixedColumns.min.js"></script>
+ <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script> 
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script> 
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script> 
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script> 
+ <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script> 
+ <script src=" https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script> 
+ <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script> 
 @endpush
   
 @if(session('status'))
@@ -151,7 +159,11 @@
 <script>
 $(document).ready( function () {
     $('#table').DataTable({
-        "scrollX": true
+        "scrollX": true,
+        dom: 'Bfrtip',
+         buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
     });
     
 } );
